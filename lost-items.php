d32f69b0afd8d0a2e050c0441974744eac37487a
ImMarw <jan.mares@student.ossp.cz>
2024-10-09 11:11:15 +0200

    idk

--- lost-items.php
+++ lost-items.php
@@ -23,8 +23,9 @@ include '../includes/functions.php';
 
     <div class="container">
         <h2>Nalezené věci</h2>
-        <a href="add-item.php">Přidat nalezený předmět</a>
-
+            <div class="add-item-button">
+                <a href="add-item.php">Přidat nalezený předmět</a>
+            </div>
         <div class="items">
             <?php
             $sql = "SELECT `name`, `description`, `found_date`, `image` FROM items";
