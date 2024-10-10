26fba273c5ea4f7854a8dc9716e22ff517022667
cyranipavel@gmail.com <your-email@example.com>
2024-10-10 18:06:27 +0200

    fixxed navbar v2

--- navbar.php
+++ navbar.php
@@ -11,13 +11,22 @@
         <a href="login.php" class="<?php if ($currentPage == 'login')
             echo 'active'; ?>">Administrace</a>
     </div>
+    <div class="nav-links" id="nav-links-small">
+        <a href="index.php" class="<?php if ($currentPage == 'index')
+            echo 'active'; ?>">Domů</a>
+        <a href="items.php" class="<?php if ($currentPage == 'items')
+            echo 'active'; ?>">Nalezené Předměty</a>
+        <a href="login.php" class="<?php if ($currentPage == 'login')
+            echo 'active'; ?>">Administrace</a>
+    </div>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
     </a>
 </div>
 <script>
+    document.getElementById("nav-links-small").style.display = "none";
     function toggleMenu() {
-        var navLinks = document.getElementById("nav-links");
+        var navLinks = document.getElementById("nav-links-small");
         var style = navLinks.style.display;
         if (style === 'none') {
             navLinks.style.display = "flex";
@@ -25,4 +34,10 @@
             navLinks.style.display = "none";
         }
     }
+
+    function updateScreenWidth() {
+        var navLinks = document.getElementById("nav-links-small").style.display = 'none';
+    }
+
+    window.addEventListener('resize', updateScreenWidth);
 </script>
