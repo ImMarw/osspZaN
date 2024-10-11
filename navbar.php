62afbac3ea7180dc55c3434a829921ea91ba6ac2
ImMarw <jan.mares@student.ossp.cz>
2024-10-11 07:13:20 +0200

    idk tbh

--- navbar.php
+++ navbar.php
@@ -1,4 +1,5 @@
 <!-- navbar.php -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="navbar">
     <div class="logo">
         <a href="index.php"><img src="lgoo.png" alt="Logo"></a>
